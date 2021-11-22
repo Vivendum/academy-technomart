6660a80e122057e8ae4c0921ec2e4d48984da934
Vivendum <vivendum@mail.ru>
2021-11-22 14:43:07 +0500

    automation: add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var server = require("browser-sync").create();
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var rigger = require("gulp-rigger");
@@ -10,6 +11,23 @@ gulp.task("build-html", function() {
     .pipe(rigger())
     .pipe(gulp.dest("build/before"))
     .pipe(gulp.dest("build/after"))
+    .pipe(server.stream());
+});
+
+gulp.task("server", function () {
+  server.init({
+    server: "build/after",
+    index: "index.html",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false,
+    browser: "firefox"
+  });
+
+  gulp.watch("source/template/**/*.html", gulp.series("build-html"));
+  gulp.watch("build/after/*.html").on("change", server.reload);
 });
 
 gulp.task("build", gulp.series("build-html"));
+gulp.task("start", gulp.series("build-html", "server"));
