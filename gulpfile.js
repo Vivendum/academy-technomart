404646be22c4ccedc6dbf2bffb34e8a7841c5f9b
Vivendum <vivendum@mail.ru>
2021-11-26 17:31:55 +0500

    automation: add test for validation html

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var server = require("browser-sync").create();
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var rigger = require("gulp-rigger");
+var validator_html = require("gulp-w3c-html-validator");
 
 gulp.task("build-html", function() {
   return gulp.src("source/template/*.html")
@@ -14,7 +15,7 @@ gulp.task("build-html", function() {
     .pipe(server.stream());
 });
 
-gulp.task("server", function () {
+gulp.task("server", function() {
   server.init({
     server: "build/after",
     index: "index.html",
@@ -29,5 +30,19 @@ gulp.task("server", function () {
   gulp.watch("build/after/*.html").on("change", server.reload);
 });
 
+gulp.task("validator-html", function() {
+  return gulp.src("build/before/*.html")
+    .pipe(validator_html())
+    .pipe(validator_html.reporter());
+});
+
+gulp.task("validator-html-min", function() {
+  return gulp.src("build/after/*.html")
+    .pipe(validator_html())
+    .pipe(validator_html.reporter());
+});
+
+gulp.task("test", gulp.series("validator-html"));
+gulp.task("test-min", gulp.series("validator-html-min"));
 gulp.task("build", gulp.series("build-html"));
 gulp.task("start", gulp.series("build-html", "server"));
