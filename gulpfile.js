5ea7200f43967f89af3d1da32072ed791d4a6d8d
Vivendum <vivendum@mail.ru>
2021-12-08 20:40:25 +0500

    automation: add deploy

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var plumber = require("gulp-plumber");
 var rigger = require("gulp-rigger");
 var validator_html = require("gulp-w3c-html-validator");
 var linter_html = require("gulp-htmlhint");
+var publish_project = require("gulp-gh-pages");
 
 gulp.task("build-html", function() {
   return gulp.src("source/template/*.html")
@@ -14,6 +15,12 @@ gulp.task("build-html", function() {
     .pipe(gulp.dest("build/before"))
     .pipe(gulp.dest("build/after"))
     .pipe(server.stream());
+});
+
+gulp.task("copy", function(done) {
+  gulp.src("build/after/*.html")
+    .pipe(gulp.dest("public/"));
+  done();
 });
 
 gulp.task("server", function() {
@@ -49,7 +56,14 @@ gulp.task("validator-html-min", function() {
     .pipe(validator_html.reporter());
 });
 
+gulp.task("publish", function() {
+  return gulp.src("./public/**/*")
+    .pipe(publish_project());
+});
+
 gulp.task("test", gulp.series("validator-html", "linter-html"));
 gulp.task("test-min", gulp.series("validator-html-min"));
 gulp.task("build", gulp.series("build-html"));
+gulp.task("public", gulp.series("build-html", "copy"));
 gulp.task("start", gulp.series("build-html", "server"));
+gulp.task("deploy", gulp.series("public", "publish"));
